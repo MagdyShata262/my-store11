3d0956739bfb1c4cea6d7154253e0e22245b7aee
Magdy Shata <magdyshata262@gmail.com>
2025-10-08 18:52:38 +0300

    add-1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
+  isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
@@ -9,9 +10,21 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { provideStore, provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import * as fromUsers from './+state/users.reducer';
+import { UsersEffects } from './+state/users.effects';
+import { UsersFacade } from './+state/users.facade';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideEffects(UsersEffects),
+    provideState(fromUsers.USERS_FEATURE_KEY, fromUsers.usersReducer),
+    UsersFacade,
+    provideStore(),
+    provideStoreDevtools({ logOnly: !isDevMode() }),
+    provideEffects(),
     provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
